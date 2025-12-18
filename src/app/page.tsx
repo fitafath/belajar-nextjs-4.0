1054469bd43360b243ca9784766395ce804805bf
Fita Fath <fitawithf@gmail.com>
2025-12-18 20:10:35 +0700

    update PostList

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,7 @@
 import Image from "next/image";
 import Link from "next/link";
-async function getLastPost () {
-  const res = await fetch ("https://jsonplaceholder.typicode.com/posts?_limit=5");
-  const posts = await res.json();
-  return posts;
-}
+import { getLastPost } from "@/lib/api";
+import PostList from "@/components/PostsList";
 
 export default async function Home() {
   const posts = await getLastPost();
@@ -14,15 +11,7 @@ export default async function Home() {
       <h1>Selamat datang di NextJS News</h1>
       <p className="subtitle">Lorem ipsum dolor sit amet consectetur adipisicing elit. Illo unde fuga error, aliquid enim doloremque impedit corrupti ullam laborum consequatur.</p>
     </div>
-    <div className="post-list">
-      {
-        posts.map((post) => (
-          <div className="post-item" key={post.id}>
-            <h2><Link href='/'>{post.title}</Link></h2>
-          </div>
-        ))
-      }
-    </div>
+    <PostList posts={posts} />
     </>
   );
 }
---
+++ src/components/PostsList.tsx
@@ -0,0 +1,23 @@
+import Link from "next/link";
+import { Post } from "@/types/post";
+
+
+interface PostsListProps {
+  posts: Post[]
+}
+
+function PostsList({posts}: PostsListProps) {
+  return (
+        <div className="post-list">
+      {
+        posts.map((post) => (
+          <div className="post-item" key={post.id}>
+            <h2><Link href='/'>{post.title}</Link></h2>
+          </div>
+        ))
+      }
+    </div>
+  )
+}
+
+export default PostsList
